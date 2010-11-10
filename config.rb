0bbacf4133f2bb31d5b46f84a0a59f4ddc7dab59
ericdfields <efields@optaros.com>
2010-11-10 17:10:19 -0500

    all jquery mobile stylesheets now partials included in master sylesheet, added manifest, updated compass conf, moved mixins to helpers dir, added default border radius to master stylesheet

--- config.rb
+++ config.rb
@@ -1,9 +1,12 @@
 # Require any additional compass plugins here.
 # Set this to the root of your project when deployed:
 http_path = "http://code.jquery.com/mobile/1.0a1"
-css_dir = "css"
+css_dir = "/"
 sass_dir = "src"
 images_dir = "images"
 javascripts_dir = "javascripts"
+line_comments = false #hawt
+# The output style for the compiled css. One of: :nested, :expanded, :compact, or :compressed.
+output_style = :compact
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
